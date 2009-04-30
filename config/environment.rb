abacbedc0b34f9a127e6b13fd25e45af6a4ebc13
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-04-30 16:39:58 +0700

    Fixe OAuth bug

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   
   config.gem "haml"
   config.gem "rdiscount"  
+  config.gem "oauth" 
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
