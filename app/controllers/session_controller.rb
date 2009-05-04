cd21edb46b4b3a95adfa025577fdc99096600047
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-05-04 11:53:11 +0700

    Fixed auth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -25,7 +25,7 @@ class SessionController < ApplicationController
     
     if oidresp.status == OpenID::Consumer::SUCCESS
       ax_resp = OpenID::AX::FetchResponse.from_success_response(oidresp)
-      email = ax_resp['http://axschema.org/contact/email'].firsts
+      email = ax_resp['http://axschema.org/contact/email'].first
       user = User.find_or_create_by_email(email)
       user.open_id = oidresp.display_identifier
       user.save(false)
